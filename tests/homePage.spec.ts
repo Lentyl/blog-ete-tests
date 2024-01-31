2fa09e7cd71dacf979659200cdc35d19b6a29405
Mariusz Podgorski <m.b.podgorski1984@gmail.com>
2024-01-31 12:33:31 +0100

    change of system in yml file

--- tests/homePage.spec.ts
+++ tests/homePage.spec.ts
@@ -28,6 +28,7 @@ test.describe('Log in suite', () => {
     await page.getByLabel('Username or email address').fill('many185@wp.pl');
     await page.getByLabel('Password').fill('Mariusz123%$');
     await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+    await page.pause();
     await page.getByRole('button', { name: 'Mariusz04' }).click()
     await expect(page.locator('.text-sm.text-gray-500')).toHaveText('many185@wp.pl');
   });
